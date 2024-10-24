b91b4450582017603cc6adae7b79f0729ce7cc22
Mike-Frohmader <mike.frohmader@sonarsource.com>
2024-10-24 13:51:32 -0500

    Update admin.py
    Trivial to trigger sonarqube

--- src/sales/admin.py
+++ src/sales/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Position, Sale, CSV
 # Register your models here.
+# Making a trivial edit to try to trigger sonarqube
 
 admin.site.register(Position)
 admin.site.register(Sale)
